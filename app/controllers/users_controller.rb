5367efb76398949c6c22f23c45f53a7544d17232
RyanandDave <yoyo>
2015-10-21 14:17:22 -0400

    Create UserController.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      # log_in!(@user)
+      redirect_to user_url
+    else
+      render :new
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:user_name, :password)
+    end
 end
